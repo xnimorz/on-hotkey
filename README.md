2fc47d0a3536c249715a887ac21ce7ec2e08bec7
Nik Mostovoy <nik.mostovoy@gmail.com>
2023-04-10 17:03:08 +0100

    Create lib

---
+++ README.md
@@ -0,0 +1,102 @@
+# On-key
+
+on-key is a small, tree-shakecable library, which simplifies the work with hotkeys on your website. 
+
+Simple example:
+```js
+import { onCodeDown, cmd, ctrl } from 'on-key';
+import { code_KeyA } from 'on-key/codes';
+
+// ...
+
+// Only keys
+onCodeDown(code_KeyA, () => {
+  console.log("A symbol was pressed");
+});
+
+// Keys with modifiers
+onCodeDown({
+    key: code_KeyA, // You also can just type 'a', instead of code
+    mods: [cmd]
+}, () => {
+  console.log("cmd+A was pressed");
+});
+
+// Handle different key combination
+onCodeDown({
+    keys: [{
+        key: 'a',
+        mods: [cmd]
+    }, {
+        key: 'a',
+        mods: [ctrl]
+    }],     
+}, () => {
+  console.log("cmd+A/ctrl+a was pressed");
+});
+
+// Handle event on custom HTML element:
+// Keys with modifiers
+onCodeDown({
+    key: code_KeyA, // You also can just type 'a', instead of code
+    mods: [cmd],
+    scope: document.querySelector('.selector'),
+}, () => {
+  console.log("cmd+A was pressed");
+});
+
+```
+
+Complex example: https://codesandbox.io/s/on-key-example-gmumel 
+
+### on-key provides 4 methods:
+
+1. `onKeyDown` - allows you to add a handler, which would check the `key` from user action and if it's matches, your callback will be called when user pressed a key
+2. `onKeyUp` - same as onKeyDown, but the callback is called when user releases a key. 
+
+`onKeyDown`, `onKeyUp` depend on user language setting. It means, that some of "hotkeys" might not work, depending on which language layout user uses. To avoid his, you can use 2 alternative methods:
+
+1. `onCodeDown` allows you to track key presses by their `code` field
+2. `onCodeUp` allows you to track key up events by their `code` field
+
+
+### Arguments:
+
+All 4 methods work with the same API:
+
+```
+onKeyDown(Options, Handler)
+```
+
+Options is either a string of the key which needs to be tracked ('a','b','f' etc.) or the object: 
+```
+{
+    key: string,
+    mods: key modifiers: ctrl, cmd, alt, shift
+    scope: HtmlElement - the event handling can be assigned to any element
+}
+```
+
+or Options can support many key handlers at once:
+```
+{
+    keys: Array<{
+        key: string,
+        mods: key modifiers: ctrl, cmd, alt, shift
+    }>,
+    scope: HtmlElement - the event handling can be assigned to any element
+}
+```
+
+The function returns `unsubscribe` handler which allows you to free handler once you don't need it:
+```js
+const unsubscribe = onCodeDown({
+    key: code_KeyA, // You also can just type 'a', instead of code
+    mods: [cmd]
+}, () => {
+  console.log("cmd+A was pressed");
+});
+// ...
+
+unsubscribe();
+```
---
+++ src/index.ts
@@ -0,0 +1,100 @@
+export const alt = 'altKey';
+export const ctrl = 'ctrlKey';
+export const meta = 'metaKey';
+export const cmd = 'metaKey';
+export const shift = 'shiftKey';
+
+export type Mod = typeof alt | typeof ctrl | typeof meta | typeof shift
+
+export type Options = SingleOption | MultipleOptions;
+
+type SingleOption = {
+    key: string,
+    mods?: Mod[],
+    scope?: HTMLElement | Window,
+};
+
+type MultipleOptions = {
+    scope?: HTMLElement | Window
+    keys: {
+        key: string,
+        mods?: Mod[]
+    }[],
+};
+
+export type KeyboardEventHandler = (e: KeyboardEvent) => unknown;
+
+export type Unsubscribe = () => void;
+
+function matchOption(e: KeyboardEvent, option: { key: string, mods?: Mod[] }, type: 'key' | 'code'): boolean {
+    if (type === 'key') {
+        if (option.key.toUpperCase() !== e.key.toUpperCase()) {
+            return false;
+        }
+    } else {
+        if (option.key.toUpperCase() !== e.code.toUpperCase()) {
+            return false;
+        }
+    }
+
+    if (option.mods == null) {
+        return true;
+    }
+
+    const mods: Mod[] = option.mods;
+    for (const modifier of mods) {
+        if (e[modifier] !== true) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function match(event: KeyboardEvent, config: Options, type: 'key' | 'code'): boolean {
+    if ('keys' in config) {
+        return config.keys.some((key) => matchOption(event, key, type));
+    }
+    return matchOption(event, { key: config.key, mods: config.mods }, type)
+}
+
+function toConfig(key: string | Options): Options {
+    if (typeof key === 'string') {
+        return {
+            key,
+        }
+    }
+    return key;
+}
+
+function onKey(key: string | Options, event: 'keyup' | 'keydown', type: 'key' | 'code', handler: KeyboardEventHandler): Unsubscribe {
+    const config = toConfig(key);
+
+    const handleKeyEvent = (e) => {
+        if (match(e, config, type)) {
+            handler(e);
+        }
+    };
+
+    (config.scope ? config.scope : window).addEventListener(event, handleKeyEvent);
+
+    return () => {
+        (config.scope ? config.scope : window).removeEventListener(event, handleKeyEvent);
+    }
+}
+
+export function onKeyUp(key: string | Options, handler: KeyboardEventHandler): Unsubscribe {
+    return onKey(key, 'keyup', 'key', handler)
+}
+
+export function onKeyDown(key: string | Options, handler: KeyboardEventHandler) {
+    return onKey(key, 'keydown', 'key', handler)
+}
+
+export function onCodeUp(key: string | Options, handler: KeyboardEventHandler) {
+    return onKey(key, 'keyup', 'code', handler)
+}
+
+export function onCodeDown(key: string | Options, handler: KeyboardEventHandler) {
+    return onKey(key, 'keydown', 'code', handler)
+}
